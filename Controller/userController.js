0f815d708fde35d5f6cb6a28c5f4ceef453d3e00
Jazzmine07 <61366865+Jazzmine07@users.noreply.github.com>
2021-10-29 16:12:36 +0800

    my current version

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,7 +1,6 @@
 //const connection = require('../database');
 const database = require('../firebase');
-const { get, onValue, push, ref, set } = require("firebase/database");
-const { getKey } = require("firebase/functions");
+const { get, onValue, push, ref, set, } = require("firebase/database");
 const bodyParser = require('body-parser');
 const urlencoder = bodyParser.urlencoded({
   extended: false
@@ -21,37 +20,47 @@ exports.login = function(req, res){
   // Adding data
   // push = with unique id
   // set = no unique id
-  var add = push(userRef, {
-    email: email,
-    pass: pass,
-  })
-  .then(() => {
-    console.log("data is added!");
-    //console.log("is dis the key? " + key);
-  })
-  .catch((error) =>{
-    console.log("error");
-    console.log(error);
-  })
+  // push(userRef, {
+  //   email: email,
+  //   pass: pass,
+  // })
+  // .then((snapchat) => {
+  //   console.log("data is added!");
+  //   var key = snapchat.key;
+  //   console.log("is dis the key? " + key);
+  // })
+  // .catch((error) =>{
+  //   console.log("error");
+  //   console.log(error);
+  // })
 
-  // Retrieving data (entire object)
-  // get(userRef).then((snapshot) => {
-  //   if (snapshot.exists()) {
-  //     console.log("Data:");
-  //     console.log(snapshot.val());
-  //   } else {
-  //     console.log("No data available");
-  //   }
-  // }).catch((error) => {
-  //   console.log("Error");
-  //   console.error(error);
-  // });
+  //Retrieving data (entire object)
+  get(userRef).then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log("Data:");
+      console.log(snapshot.val());
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.log("Error");
+    console.error(error);
+  });
 
-  // Get key
+  // getting table userRef = user
   onValue(userRef, (snapshot) => {
     console.log("onValue:");
     console.log(snapshot.val());
-    console.log("snapchat:");
-    console.log(snapshot.key);
+    if(snapchat.childSnapshot("email")){
+      console.log("email?:");
+      console.log(snapshot.val());
+    }
   })
-};
+
+  onValue(userRef, (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+      const childData = childSnapshot.val();
+      console.log("childata " + childData);
+    });
+  })
+}

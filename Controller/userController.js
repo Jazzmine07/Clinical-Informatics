31c4a69203fbdb28a2610a875433d99bf89f3612
Jazzmine07 <61366865+Jazzmine07@users.noreply.github.com>
2021-11-12 14:27:55 +0800

    connected front-end

--- Controller/userController.js
+++ Controller/userController.js
@@ -14,7 +14,37 @@ exports.login = function(req, res){
   var database = firebase.database();
   var userRef = database.ref("user");
 
-  // create user with email and password in firebase authentication (wont reflect sa real time database)
+  // var studentAccount = {
+  //   idNum: '275755',
+  //   password: 'manresa123',
+  // }
+  // database.ref('users/' + studentAccount.idNum); // setting the path with id number as its pk
+  // database.ref('users/' + studentAccount.idNum).set(studentAccount); // adding other fields
+
+  // var personalInfo = {
+  //   idNum: '275755',
+  //   firstName: 'Levi',
+  //   middleName: 'Lopez',
+  //   lastName: 'Torres',
+  //   studentType: 'old',
+  //   grade: '2',
+  //   section: 'truthfulness',
+  //   birthday: '2013-11-15', // yyyy-mm-dd
+  //   age: '8',
+  //   sex: 'Male',
+  //   address: 'Metro Manila',
+  //   motherName: 'Chloe Torres',
+  //   motherEmail: 'chloe_torres@gmail.com',
+  //   fatherName: 'Antonio Torres',
+  //   fatherEmail: 'antonio_torres@gmail.com',
+  //   guardianName: 'Chloe Torres',
+  //   guardianEmail: 'chloe_torres@gmail.com',
+  // }
+  // database.ref('studentInfo/' + personalInfo.idNum); // setting the path with id number as its pk
+  // database.ref('studentInfo/' + personalInfo.idNum).set(personalInfo); // adding other fields
+
+
+  //create user with email and password in firebase authentication (wont reflect sa real time database)
   // firebase.auth().createUserWithEmailAndPassword(email, pass) 
   // .then((userCredential) => {
   //   console.log("user " + userCredential.user);
@@ -24,8 +54,8 @@ exports.login = function(req, res){
   //     firstName: "Dr. Joseph",
   //     lastName: "Ayo"
   //   }
-  //   database.ref('user/' + userCredential.user.uid); // setting the path with uid as its pk
-  //   database.ref('user/' + userCredential.user.uid).set(update); // adding fields such as email, firstname and lastname
+  //   database.ref('users/' + userCredential.user.uid); // setting the path with uid as its pk
+  //   database.ref('users/' + userCredential.user.uid).set(update); // adding fields such as email, firstname and lastname
   // })
   // .catch((error) => {
   //   var errorCode = error.code;
@@ -35,73 +65,81 @@ exports.login = function(req, res){
   //   console.log(errorMessage);
   // });
 
-  // user sign in
-  firebase.auth().signInWithEmailAndPassword(email, pass)
-  .then((userCredential) => {
+  if(email == "" && pass == ""){
+    res.render('login',{
+      error: true,
+      error_msg: "Please enter data!"
+    });
+  }
+  else {
+    // user sign in
+    firebase.auth().signInWithEmailAndPassword(email, pass)
+    .then((userCredential) => {
 
-    //How to get specific field
-    var query = userRef.child(userCredential.user.uid);
-    query.on('value', (snapshot) => {
-      console.log("result:");
-      console.log(snapshot.val());
+      //How to get specific field
+      var query = userRef.child(userCredential.user.uid);
+      query.on('value', (snapshot) => {
+        console.log("result:");
+        console.log(snapshot.val());
 
-      console.log("email: " + snapshot.child('email').val());
-      console.log("firstName: " + snapshot.child('firstName').val());
-      console.log("lastName: " + snapshot.child('lastName').val());
+        console.log("email: " + snapshot.child('email').val());
+        console.log("firstName: " + snapshot.child('firstName').val());
+        console.log("lastName: " + snapshot.child('lastName').val());
 
-      var user = {
-        email: snapshot.child('email').val(),
-        firstName: snapshot.child('firstName').val(),
-        lastName: snapshot.child('lastName').val()
-      }
+        var user = {
+          email: snapshot.child('email').val(),
+          firstName: snapshot.child('firstName').val(),
+          lastName: snapshot.child('lastName').val()
+        }
+        
+        //res(user);
+        // var pk = snapshot;
+        // console.log("is dis the key? " + pk);
 
-      res.render('index');
-      
-      //res(user);
-      // var pk = snapshot;
-      // console.log("is dis the key? " + pk);
+        // if(snapshot.key.child("email")){
+        //     console.log("email?: ");
+        //     console.log(snapshot.val());
+        // }
 
-      // if(snapshot.key.child("email")){
-      //     console.log("email?: ");
-      //     console.log(snapshot.val());
-      // }
+        res.redirect('/dashboard'); // redirect to dashboard page
+      })
     })
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
 
-    if (errorCode === 'auth/wrong-password') {
-      res.render('login',{
-        error: true,
-        error_msg: "Wrong password!"
-      });
-    } 
-    else if (errorCode === 'auth/invalid-email') {
-      res.render('login',{
-        error: true,
-        error_msg: "Please enter a valid email!"
-      });
-    } 
-    else if (errorCode === 'auth/user-not-found') {
-      res.render('login',{
-        error: true,
-        error_msg: "No user with such email!"
-      });
-    } 
-    else if (errorCode === 'auth/user-disabled') {
-      res.render('login',{
-        error: true,
-        error_msg: "Account disabled by Admin!"
-      });
-    }
-    else {  // in the case of multiple login failed attempts
-      res.render('login',{
-        error: true,
-        error_msg: errorMessage 
-      });
-    }
-  });
+      if (errorCode === 'auth/wrong-password') {
+        res.render('login',{
+          error: true,
+          error_msg: "Wrong password!"
+        });
+      } 
+      else if (errorCode === 'auth/invalid-email') {
+        res.render('login',{
+          error: true,
+          error_msg: "Please enter a valid email!"
+        });
+      } 
+      else if (errorCode === 'auth/user-not-found') {
+        res.render('login',{
+          error: true,
+          error_msg: "No user with such email!"
+        });
+      } 
+      else if (errorCode === 'auth/user-disabled') {
+        res.render('login',{
+          error: true,
+          error_msg: "Account disabled by Admin!"
+        });
+      }
+      else {  // in the case of multiple login failed attempts
+        res.render('login',{
+          error: true,
+          error_msg: errorMessage 
+        });
+      }
+    });
+  }
 
   
 
--- routes/index.js
+++ routes/index.js
@@ -16,11 +16,26 @@ router.get('/', (req, res) => {
 
   // Get dashboard page
   router.get('/dashboard', (req, res) => {
-    //if (req.session.user) res.redirect('/POS');
-    //else {
       console.log("Read dashboard successful!");
-      res.render('index');
-    //}
+      res.render('dashboard');
+  });
+
+  // Get clinic visit page
+  router.get('/clinic-visit', (req, res) => {
+      console.log("Read dashboard successful!");
+      res.render('clinic-visit');
+  });
+
+  // Get clinic visit page
+  router.get('/clinic-visit/create', (req, res) => {
+    console.log("Read create clinic visit successful!");
+    res.render('clinic-visit-create');
+  });
+
+  // Get profile page
+  router.get('/profile', (req, res) => {
+    console.log("Read profile successful!");
+    res.render('profile');
   });
 
 router.post('/login', userController.login);

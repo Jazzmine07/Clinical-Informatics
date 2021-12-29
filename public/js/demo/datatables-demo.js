0f1506e4bdfb7804d648d27d2904654d25bb33c2
kressha <61380550+kressha@users.noreply.github.com>
2021-12-30 02:42:31 +0800

    grouped inventory, updated display in profile

--- public/js/demo/datatables-demo.js
+++ public/js/demo/datatables-demo.js
@@ -24,15 +24,70 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-  $('#medicineTable').DataTable();
+  $('#usedMedicineTable').DataTable();
 });
 
 $(document).ready(function() {
-  $('#supplyTable').DataTable();
+  //$('#medicineTable').DataTable();
+  $('#medicineTable').DataTable({
+    paging: false,
+    order: [[1, 'asc']],
+    rowGroup: {
+        endRender: function (rows, group ) {
+            var total = rows
+                .data()
+                .pluck(2)
+                .reduce( function (a, b) {
+                    return a + b.replace(/[^\d]/g, '')*1;
+                }, 0);
+
+            return ' Total Quantity: '+
+                $.fn.dataTable.render.number(',', '.', 0).display( total );
+        },
+        dataSrc: 1
+    }
+  });
 });
 
 $(document).ready(function() {
-  $('#dentalTable').DataTable();
+  $('#supplyTable').DataTable({
+    order: [[1, 'asc']],
+    rowGroup: {
+        endRender: function (rows, group ) {
+            var total = rows
+                .data()
+                .pluck(2)
+                .reduce( function (a, b) {
+                    return a + b.replace(/[^\d]/g, '')*1;
+                }, 0);
+
+            return ' Total Quantity: '+
+                $.fn.dataTable.render.number(',', '.', 0).display( total );
+        },
+        dataSrc: 1
+    }
+  });
+});
+
+$(document).ready(function() {
+  $('#dentalTable').DataTable({
+    pageLength: 100,
+    order: [[1, 'asc']],
+    rowGroup: {
+        endRender: function (rows, group ) {
+            var total = rows
+                .data()
+                .pluck(2)
+                .reduce( function (a, b) {
+                    return a + b.replace(/[^\d]/g, '')*1;
+                }, 0);
+
+            return ' Total Quantity: '+
+                $.fn.dataTable.render.number(',', '.', 0).display( total );
+        },
+        dataSrc: 1
+    }
+  });
 });
 
 $(document).ready(function() {
